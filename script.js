2586a2ab46f5e32b1658d43474400d4ba1d0d807
tibor97 <tibor.serencei_vuk@yahoo.com>
2020-11-20 13:33:44 +0100

    add tabs section

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var navbarListItems = document.querySelectorAll('.c-navbar-link');
 
 hamburgerMenu.onclick = function() {
     navbarList.classList.toggle('active');
-    navbarList.classList.add('transition');
+    // navbarList.classList.add('transition');
     hamburgerMenu.classList.toggle('c-hamburger-menu--toggle');
 }
 
 nav.ontransitionend = function() {
-    navbarList.classList.remove('transition');
+    // navbarList.classList.remove('transition');
 }
 
 navbarListItems.forEach(function(navItems) {
